434abe0a9d9e51d18748fdd23b3d64bc4be08778
mikkkey96 <dineeb1996@gmail.com>
2025-09-03 14:44:50 +0100

    Add vite display

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: true
+  },
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: true
   }
 })
